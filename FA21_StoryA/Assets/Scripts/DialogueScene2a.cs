56f9d60ca80770af71141bf8465e46f529f506d5
Fionabellcamp <fcampbell2001@gmail.com>
2021-10-17 14:37:10 -0400

    Adding pause menus to scenes

--- FA21_StoryA/Assets/Scripts/DialogueScene2a.cs
+++ FA21_StoryA/Assets/Scripts/DialogueScene2a.cs
@@ -23,7 +23,7 @@ public class DialogueScene2a : MonoBehaviour {
         public GameObject NextScene2Button;
         public GameObject nextButton;
         public GameHandler gameHandler;
-        public AudioSource audioSource;
+        //public AudioSource audioSource;
         private bool allowSpace = true;
 
 void Start(){         // initial visibility settings
--- FA21_StoryA/Assets/Scripts/GameHandler.cs
+++ FA21_StoryA/Assets/Scripts/GameHandler.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
 
 public class GameHandler : MonoBehaviour{
 
@@ -14,18 +16,59 @@ public class GameHandler : MonoBehaviour{
 		public static bool gotBearHelp = false;
 	    public static bool ateBerries = false;
 		public static bool newDiscovery = false;
+		 public static bool GameisPaused = false;
+        public GameObject pauseMenuUI;
+        public AudioMixer mixer;
+        public static float volumeLevel = 1.0f;
+        private Slider sliderVolumeCtrl;
 		
+		//pause menu stuff
+		void Awake (){
+                SetLevel (volumeLevel);
+                GameObject sliderTemp = GameObject.FindWithTag("PauseMenuSlider");
+                if (sliderTemp != null){
+                        sliderVolumeCtrl = sliderTemp.GetComponent<Slider>();
+                        sliderVolumeCtrl.value = volumeLevel;
+                }
+        }
+		
+		     void Start (){
+                pauseMenuUI.SetActive(false);
+        }
+
+        void Update (){
+                if (Input.GetKeyDown(KeyCode.Escape)){
+                        if (GameisPaused){
+                                Resume();
+                        }
+                        else{
+                                Pause();
+                        }
+                }
+        }
+
+        void Pause(){
+                pauseMenuUI.SetActive(true);
+                Time.timeScale = 0f;
+                GameisPaused = true;
+        }
+
+        public void Resume(){
+                pauseMenuUI.SetActive(false);
+                Time.timeScale = 1f;
+                GameisPaused = false;
+        }
+
+        public void SetLevel (float sliderValue){
+                mixer.SetFloat("MusicVolume", Mathf.Log10 (sliderValue) * 20);
+                volumeLevel = sliderValue;
+        }
 		
         //public GameObject textGameObject;
 
         //void Start () { UpdateScore (); }
 
-        void Update(){         //delete this quit functionality when a Pause Menu is added
-                if (Input.GetKey("escape")){
-                        Application.Quit();
-                }
-        }
-
+        
 //assign true to static variables
 		public  void UpdateOwl(){
                 beenToOwl = true;
@@ -106,8 +149,11 @@ public class GameHandler : MonoBehaviour{
 		
 		
         public void RestartGame(){
+			 Time.timeScale = 1f;
                 SceneManager.LoadScene("MainMenu");
         }
+		
+		
 
         public void QuitGame(){
                 #if UNITY_EDITOR
